ef3206c197c8556110366c962a010a8d26d668d6
jannahuang <jannajanna@126.com>
2022-07-24 11:16:45 +0800

    Dialog基本样式

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,14 +1,27 @@
 <template>
-  <div>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-  </div>
+  <div>Dialog 示例</div>
+  <h1>示例1</h1>
+  <Button @click="toggle">toggle</Button>
+  <Dialog :visible="show"></Dialog>
 </template>
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import { ref } from 'vue'
+export default {
+  components: {
+    Dialog,
+    Button
+  },
+  setup() {
+    const show = ref(false)
+    const toggle = () => {
+      show.value = !show.value
+    }
+    return {
+      show,
+      toggle
+    }
+  }
+}
+</script>
